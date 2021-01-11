8941c710d981c7b55086c28868cf6341cd34e1f7
Tami Takada <tami@mytalos.com>
2021-01-11 16:42:29 -0500

    added simple sort

--- Radix.java
+++ Radix.java
@@ -18,4 +18,33 @@ public class Radix {
     }
   }
 
+  public static void radixSortSimple(SortableLinkedList data) {
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i++) {
+      buckets[i] = new SortableLinkedList();
+    }
+
+    int maxLen = 1;
+    for (int i = data.size() - 1; i >= 0; i--) {
+      if (length(data.get(i)) > maxLen) maxLen = length(data.get(i));
+      buckets[nth(data.get(i), 0)].add(data.get(i));
+      data.remove(i);
+    }
+
+    merge(data, buckets);
+
+    for (int i = 1; i <= maxLen; i++) {
+      for (int j = 0; j < data.size(); j++) {
+        if (length(data.get(j)) <= i) buckets[0].add(data.get(j));
+        else {
+          buckets[nth(data.get(j), i)].add(data.get(j));
+        }
+        data.remove(j);
+        j--;
+      }
+
+      merge(data, buckets);
+    }
+  }
+
 }
